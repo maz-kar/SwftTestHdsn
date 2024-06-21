d752582f644d4c555911ee412efff748a99bf40b
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-21 08:46:40 +0200

    Two tests for methods added.

--- SwftTestHudsonTests/SwftTestHudsonTests.swift
+++ SwftTestHudsonTests/SwftTestHudsonTests.swift
@@ -10,28 +10,28 @@ import XCTest
 
 final class SwftTestHudsonTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        // Any test you write for XCTest can be annotated as throws and async.
+//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
     func testHaterStartNicely() {
         let hater = Hater()
@@ -39,5 +39,20 @@ final class SwftTestHudsonTests: XCTestCase {
         XCTAssertEqual(hater.hating, false)
     }
     
+    func testHaterHadABadDay() {
+        var hater = Hater()
+        
+        hater.hadABadDay()
+        
+        XCTAssertTrue(hater.hating)
+    }
+    
+    func testHaterHadAGoodDay() {
+        var hater = Hater()
+        
+        hater.hadAGoodDay()
+        
+        XCTAssertFalse(hater.hating)
+    }
 
 }
