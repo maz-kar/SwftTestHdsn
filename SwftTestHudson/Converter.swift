44300bb390237604cb006d2d2fee91a4c9f5b3d5
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-07-04 08:22:36 +0200

    converter struct was created.

---
+++ SwftTestHudson/Converter.swift
@@ -0,0 +1,19 @@
+//
+//  Converter.swift
+//  SwftTestHudson
+//
+//  Created by Maziar Layeghkar on 03.07.24.
+//
+
+import Foundation
+
+struct Converter {
+    
+    func convertToCelcius(fahrenheit: Double) -> Double {
+        return (fahrenheit - 32) * 5/9
+    }
+    
+}
+
+
+
